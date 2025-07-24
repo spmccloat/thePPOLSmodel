54ccfc0aba1e80621e578875339234e8fedd3cf5
spmccloat <spmccloat@users.noreply.github.com>
2025-07-24 11:28:28 -0400

    Update conf.py
    try: autodoc_mock_imports to actually populate the content

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,11 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+autodoc_mock_imports = [
+	"astropy",
+	"matplotlib"
+	]
+
 
 
 # -- Options for HTML output -------------------------------------------------
